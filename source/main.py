e56b177ee296cbbea0e96bf4e79413736b31016f
woXrooX <woXrooX@gmail.com>
2024-10-12 19:01:10 +0500

    URL with param sample

--- source/main.py
+++ source/main.py
@@ -7,3 +7,7 @@ def home(): return "Welcome to BEE!"
 
 @app.route('/about')
 def about(): return "About BEE"
+
+@app.route('/user/<username>/settings')
+def user_profile(username):
+	return f"<h1>user > {username} > settings</h1>"
