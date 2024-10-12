a3cdd9a31752b66b72a5ee517c6e975e2476e9a8
woXrooX <woXrooX@gmail.com>
2024-10-12 19:00:56 +0500

    URL params added

--- source/BEE/BEE.py
+++ source/BEE/BEE.py
@@ -2,26 +2,57 @@ if __name__ != "__main__":
 	import re
 
 	class BEE:
+		#### Defaults
+
 		def __init__(self):
-			# "path": "func"
-			self.routes = {}
+			self.routes = {
+				# "pattern" : {
+				# 	"handler_func": handler_func,
+				# 	"URL_params": URL_params
+				# }
+			}
 
 		def __call__(self, environ, start_response):
 			environ_path = environ['PATH_INFO']
 
-			for path, func in self.routes.items():
-				match = path.match(environ_path)
+			for pattern, route_info in self.routes.items():
+
+				match = pattern.match(environ_path)
 
 				if match:
+					kwargs = dict(zip(route_info["URL_params"], match.groups()))
 					start_response('200 OK', [('Content-Type', 'text/html')])
-					return [func().encode()]
+					return [route_info["handler_func"](**kwargs).encode()]
 
 			start_response('404 Not Found', [('Content-Type', 'text/plain')])
 			return [b'404 Not Found']
 
+
+
+
+		#### Decorators
+
 		def route(self, path):
-			def decorator(func):
-				self.routes[re.compile(f"^{path}$")] = func
-				return func
+			def decorator(handler_func):
+				pattern, URL_params = self.compile_route_path(path)
+
+				self.routes[pattern] = {
+					"handler_func": handler_func,
+					"URL_params": URL_params
+				}
+
+				return handler_func
 
 			return decorator
+
+
+
+
+		#### Helpers
+
+		def compile_route_path(self, path):
+			param_pattern = r'<([^>]+)>'
+			URL_params = re.findall(param_pattern, path)
+			regex_pattern = re.sub(param_pattern, r'([^/]+)', path)
+			return re.compile(f"^{regex_pattern}$"), URL_params
+
