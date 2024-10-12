96579b812a3deddde7076e313f27ff35500ea8de
woXrooX <woXrooX@gmail.com>
2024-10-12 18:23:57 +0500

    clean up

--- source/BEE/BEE.py
+++ source/BEE/BEE.py
@@ -3,25 +3,25 @@ if __name__ != "__main__":
 
 	class BEE:
 		def __init__(self):
-			# "path": "handler_func"
+			# "path": "func"
 			self.routes = {}
 
 		def __call__(self, environ, start_response):
 			environ_path = environ['PATH_INFO']
 
-			for path, handler_func in self.routes.items():
+			for path, func in self.routes.items():
 				match = path.match(environ_path)
 
 				if match:
 					start_response('200 OK', [('Content-Type', 'text/html')])
-					return [handler_func().encode()]
+					return [func().encode()]
 
 			start_response('404 Not Found', [('Content-Type', 'text/plain')])
 			return [b'404 Not Found']
 
 		def route(self, path):
-			def decorator(handler_func):
-				self.routes[re.compile(f"^{path}$")] = handler_func
-				return handler_func
+			def decorator(func):
+				self.routes[re.compile(f"^{path}$")] = func
+				return func
 
 			return decorator
