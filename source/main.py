d1d99cc06fdd23eebb0f0f70e9ec15402c7bc4c2
woXrooX <woXrooX@gmail.com>
2025-06-05 08:46:52 +0500

    More test cases

--- source/main.py
+++ source/main.py
@@ -3,12 +3,9 @@ from BEE.BEE import BEE
 app = BEE()
 
 @app.route("/")
+@app.route("/home")
 def home():
 	return "Welcome to BEE!"
-
-@app.route("/about")
-def about():
-	return "About BEE"
 
 @app.route("/user/<URL_param>", methods=["GET"])
 def user_profile(URL_param):
