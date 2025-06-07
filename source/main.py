c8041c29676f74b149de532371777b76201b6b3b
woXrooX <woXrooX@gmail.com>
2025-06-07 07:53:54 +0500

    Style

--- source/main.py
+++ source/main.py
@@ -1,22 +1,19 @@
-from BEE import BEE, session
+from BEE import BEE, session, Response
 
-app = BEE()
+Bee = BEE()
 
-@app.route("/")
-@app.route("/home")
-def home():
-	return "Welcome to BEE!"
+@Bee.route("/")
+@Bee.route("/home")
+def home(): return "Welcome to BEE!"
 
-@app.route("/user/<URL_param>", methods=["GET"])
-def user_profile(URL_param):
-	return f"URL_param: {URL_param}"
+@Bee.route("/user/<URL_param>", methods=["GET"])
+def user_profile(URL_param): return f"URL_param: {URL_param}"
 
-@app.route("/visit")
+@Bee.route("/visit")
 def visit(request):
-	count = request.session.get("count", 0) + 1
-	request.session["count"] = count
+	count = session.get("count", 0) + 1
+	session["count"] = count
 	return Response.JSON({"visits": count})
 
 
-if __name__ == "__main__":
-	app.run()
+if __name__ == "__main__": Bee.run()
