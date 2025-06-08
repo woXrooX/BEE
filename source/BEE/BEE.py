2906475436b59576b375edacf1b0887fd1506196
woXrooX <woXrooX@gmail.com>
2025-06-08 08:19:04 +0500

    Reflect to Response changes

--- source/BEE/BEE.py
+++ source/BEE/BEE.py
@@ -1,7 +1,7 @@
 from wsgiref.simple_server import make_server
 
 from .Router import Router
-from .Request import Request
+from .Request import Request, BEE_ERROR_request_payload_too_large
 from .Response import Response
 from .Session.Session_Manager import Session_Manager
 from .Session.Session_Proxy import Session_Proxy
@@ -22,7 +22,7 @@ class BEE:
 
 	def route(self, path: str, methods=("GET",)):
 		def decorator(func):
-			for m in methods: self.router.register_route(path, m.upper(), func)
+			for method in methods: self.router.register_route(path, method.upper(), func)
 			return func
 
 		return decorator
@@ -31,30 +31,41 @@ class BEE:
 		request = Request(environ, self.MAX_BODY_SIZE)
 		session = self.session_manager.load(environ)
 		token = Session_Proxy.bind(session)
+		response = None
 
 		try:
-			handler, params = self.router.match(request.get_path(), request.get_method())
-			if handler is None: response = Response.not_found()
+			route_handler_func, params = self.router.match(request.get_path(), request.get_method())
+
+			if route_handler_func is None:
+				response = Response(
+					b"Not Found",
+					"404 NOT FOUND",
+					[("Content-Type", "text/plain")]
+				)
+
 			else:
 				# Handler may declare signature (request, **params) or (**params)
-				try: result = handler(request, **params)
-				except TypeError: result = handler(**params)
+				try: return_value = route_handler_func(request, **params)
+				except TypeError: return_value = route_handler_func(**params)
 
-				# Normalize result into Response
-				if isinstance(result, Response): response = result
+				if return_value is None:
+					response = Response(
+						b"No Response",
+						"444 NO RESPONSE",
+						[("Content-Type", "text/plain; charset=utf-8")]
+					)
 
-				elif isinstance(result, tuple):
-					status, headers, body = result
-					response = Response(body, status, headers)
-
-				else: response = Response.ok(result)
+				else:
+					response = Response(
+						return_value,
+						"200 OK",
+						[("Content-Type", "text/plain")]
+					)
 
 			# Persist session if needed
 			self.session_manager.save(session, response)
 
-			return response(start_response)
-
-		except RequestPayloadTooLarge:
+		except BEE_ERROR_request_payload_too_large:
 			response = Response(
 				b"Request entity too large",
 				"413 REQUEST ENTITY TOO LARGE",
@@ -63,7 +74,7 @@ class BEE:
 
 		finally: Session_Proxy.unbind(token)
 
-		return Response.generate_start_response(start_response, status, headers, body)
+		return response(start_response)
 
 	def run(self, host="127.0.0.1", port=8000):
 		with make_server(host, port, self) as server:
