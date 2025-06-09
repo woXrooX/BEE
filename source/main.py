e4df8832369b8cbca859ca796d938f35c099f749
woXrooX <woXrooX@gmail.com>
2025-06-09 14:41:38 +0500

    Update

--- source/main.py
+++ source/main.py
@@ -4,16 +4,30 @@ Bee = BEE()
 
 @Bee.route("/")
 @Bee.route("/home")
-def home(): return "Welcome to BEE!"
+def home(request):
+	print(request.get_JSON())
+	return "Welcome to BEE!"
 
 @Bee.route("/user/<URL_param>", methods=["GET"])
 def user_profile(URL_param): return f"URL_param: {URL_param}"
 
+@Bee.route("/log_in/<PWD>", methods=["GET"])
+def log_in_PWD(PWD):
+	if PWD == "JEFF":
+		session["user"] = {
+			"username": "My_name_is_JEFF",
+			"id": 12345
+		}
+
+		return f"Logged in: {session}"
+
+	else: return f"Invalid PWD"
+
 @Bee.route("/visit")
 def visit(request):
-	count = session.get("count", 0) + 1
-	session["count"] = count
-	return Response.JSON({"visits": count})
+	count = session["user"].get("count", 0) + 1
+	session["user"]["count"] = count
+	return f"Visits: {session}"
 
 
 if __name__ == "__main__": Bee.run()
